c38071d154bbcf4f05f4e09f44fec2f48bc34933
Julien Gormotte <jgormotte@ate.info>
2021-05-07 11:52:21 +0200

    uglifypy

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import sys
 
+
+class PythonVersionError(Exception):
+    pass
+
+
+if sys.version_info < (3, 6):
+    raise PythonVersionError("Python >= 3.6 required.")
+
 MESSAGE = "AZY TA SOEUR ELLE CROIT QU'UNE ERREUR 808 C'EST 2 ERREURS 404 !"
 
-print(MESSAGE)
-sys.exit(42)
+
+if __name__ == "__main__":
+    print(f"{MESSAGE}")
+    sys.exit(42)
